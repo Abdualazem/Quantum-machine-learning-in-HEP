0a0da884d577e9200b4fc5d8cfba2b817e5febd0
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2023-01-22 14:21:12 +0100

    Add simple Sphinx architecture

---
+++ conf.py
@@ -0,0 +1,15 @@
+project = 'Quantum Machine Learning'
+author = 'Abdualazem Fadol'
+copyright = '2023, Abdualazem Fadol'
+
+exclude_patterns = [
+    "_build",
+    "featureMap.ipynb",
+    "PrepareData.ipynb",
+    "README.md",
+    "Variables.ipynb",
+]
+extensions = [
+    "myst_nb",
+]
+html_theme = 'alabaster'
---
+++ index.md
@@ -0,0 +1,5 @@
+# Quantum Machine Learning Tutorial
+
+```{toctree}
+classification
+```
