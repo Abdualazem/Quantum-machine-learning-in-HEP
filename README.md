5cd7f563e3dce6d05d4ed738a14032c5c52d2ba9
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2023-01-22 15:00:49 +0100

    Build documentation with Sphinx (#1)
    * Add Conda environment config
    
    * Delete `.ipynb_checkpoints` folder
    
    * Remove `images` output folder
    
    * Add simple Sphinx architecture
    
    * FIX: create `images/` directory
    
    Needed to write the output from the notebooks
    
    * Switch to `sphinx-book-theme`
    
    See following links:
    - https://sphinx-book-theme.readthedocs.io
    - https://myst-parser.readthedocs.io/en/latest/syntax/optional.html#dollar-delimited-math
    
    * FIX: add back embedded images
    
    * Activate MyST-NB cache
    
    * Add set-up instructions in main README

---
+++ README.md
@@ -0,0 +1,21 @@
+# Quantum Machine Learning Tutorial
+
+## How to set up?
+
+Install all the required packages with Conda ([Miniconda](https://docs.conda.io/en/latest/miniconda.html)) as follows:
+
+```shell
+conda env create
+```
+
+You can now build the documentation by running:
+
+```
+make html
+```
+
+Notebooks are best edited in [Jupyter Lab](https://jupyterlab.readthedocs.io) as follows:
+
+```shell
+jupyter lab
+```
---
+++ conf.py
@@ -0,0 +1,21 @@
+project = 'Quantum Machine Learning'
+author = 'Abdualazem Fadol'
+copyright = '2023, Abdualazem Fadol'
+
+exclude_patterns = [
+    "_build",
+    "featureMap.ipynb",
+    "PrepareData.ipynb",
+    "README.md",
+    "Variables.ipynb",
+]
+extensions = [
+    "myst_nb",
+]
+html_theme = "sphinx_book_theme"
+myst_enable_extensions = [
+    "amsmath",
+    "dollarmath",
+    "html_image",
+]
+nb_execution_mode = "cache"
